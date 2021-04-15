9f03ff17e60c71926ff158df0b9bd33aa25530dc
Filmptz <you@example.com>
2021-04-15 23:15:04 +0700

    Update filename

--- back_node/server.js
+++ back_node/server.js
@@ -1,94 +1,27 @@
+const { Router } = require('express')
 var express = require('express')
 var app = express()
-var fs = require('fs') //Read user.json
 
-app.get('/getUsers', function(req,res){
-    fs.readFile(__dirname+"/"+"users.json", 'utf8', function(err,data){
-        console.log(data) // user.json data
-        res.end(data)
-    })
+port = process.env.PORT || 3000
+var mongoose = require('mongoose')
+
+Contact = require('./api/model/contactListModel')
+bodyParser = require('body-parser')
+mongoose.Promise = global.Promise
+
+var mongo_uri = 'mongodb+srv://admin:admin@contactdb.gc4ab.mongodb.net/ContactList?retryWrites=true&w=majority'
+mongoose.connect(mongo_uri,{ useNewUrlParser: true, useUnifiedTopology: true },function(err){
+    if(err) throw err
+    console.log('Sucessfully connected')
 })
 
-/*========== Get by parameter ==============*/
-app.get('/getUsers/:id', function(req,res){
-    fs.readFile(__dirname+"/"+"users.json", 'utf8', function(err,data){
-        var users = JSON.parse(data) // String => object
-        var user = users["user"+req.params.id]
-        res.end(JSON.stringify(user))
-        console.log(data) // user.json data
-        //res.end(data)
-    })
-})
+const cors = require('cors') // cross side origin
+app.use(cors())
 
-/*========== ADD User by obj ==============*/
-app.post('/addUsers', function(req,res){
-    fs.readFile(__dirname+"/"+"users.json", 'utf8', function(err,data){
-        var users = JSON.parse(data) // String => object
-        var user = {
-                            "name": "Film",
-                            "password": "1234",
-                            "occupation": "engineer",
-                            "id": "4"
-                 }
-                                    
-        users["user4"] = user
-        res.send(JSON.stringify(users))
-        console.log(data) // user.json data
-    })
-})
+app.use(bodyParser.urlencoded({extended: true})) // user body-parser for input json data
+app.use(bodyParser.json())
 
-/*========== ADD User by obj to file ==============*/
-/* app.post('/addUsers', function(req,res){
-    fs.readFile(__dirname+"/"+"users.json", 'utf8', function(err,data){
-        var users = JSON.parse(data) // String => object
-        var user = {"users4":{
-                            "name": "Film",
-                            "password": "1234",
-                            "occupation": "engineer",
-                            "id": "4"
-                            }         
-                 }
-        fs.writeFileSync(__dirname+"/"+"users.json",JSON.stringify(user),function(err,data){
-            console.log(data)
-        })                     
-        users["user4"] = user
-        res.send(JSON.stringify(users))
-        console.log(data) // user.json data
-    })
-}) */
-
-/*========== Delete User by id ==============*/
-app.delete('/delUsers/:id', function(req,res){
-    fs.readFile(__dirname+"/"+"users.json", 'utf8', function(err,data){
-        var users = JSON.parse(data) // String => object
-        delete users['user'+req.params.id]
-        console.log(users) // user.json data
-        res.end(JSON.stringify(users))
-        
-    })
-})
-
-/*========== Edit User by id ==============*/
-/* app.put('/addUsers', function(req,res){
-    fs.readFile(__dirname+"/"+"users.json", 'utf8', function(err,data){
-        var users = JSON.parse(data) // String => object
-        delete users['user'+req.params.id]
-        console.log(data) // user.json data
-        res.end(JSON.stringify(users))
-        
-    })
-}) */
-
-var server = app.listen(5000, function(){
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Application run at http://%s:%s", host, port)
-})
-
-/* To user json-server (run cmd administrator[alt+shift+enter]) inner must be list / array
-json-server can 
-            GET POST
-            PUT DELETE (by id)
-... npm install g- json-server
-... json-server --watch <filename.json> 
-*/
+var route = require('./api/routes/contactListRoutes')
+route(app)
+app.listen(port)
+console.log('User List Server started on : '+port)
