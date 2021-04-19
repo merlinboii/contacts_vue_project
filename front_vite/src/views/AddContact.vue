bc40fd3c2bf0f98a3790dfae01b39f6a08749cc1
Filmptz <you@example.com>
2021-04-19 18:56:11 +0700

    Update add

--- front_vite/src/views/AddContact.vue
+++ front_vite/src/views/AddContact.vue
@@ -80,7 +80,6 @@ export default {
           axios.post('http://localhost:5000/contacts/add/',newContact)
             .then((response)=>{
                 console.log(response.data)
-               // this.Contacts = response.data // .data is default prop of response
              })
             .catch((error)=>{
                 console.log(error) // should have table name of msg Error
