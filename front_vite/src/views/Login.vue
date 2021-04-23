029097800c72e5bbbe5b622bd1edcdee6b50dd62
Filmptz <you@example.com>
2021-04-24 00:01:56 +0700

    Add login page

---
+++ front_vite/src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container">
+        <h1>Log In</h1>
+        <br><br>
+        <div class="row">
+            <div class="col-lg-3"/>
+            <div class="col-lg-6">
+                <input type="username" v-model="formData.username" class="form-control" placeholder="username"/> 
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="password" v-model="formData.password" class="form-control" placeholder="password"/>
+                <br> 
+                <button class="btn btn-success btn-block full-width" @click="logIn">Log In</button>
+            </div>
+            <div class="col-lg-3"/>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'SignIn',
+    data() {
+        return {
+            formData: {
+                username: '',
+                password: ''
+            },
+            errorMessgae: '',
+            succeessMessage: ''
+        }
+    },
+    methods: {
+        logIn() {
+            if (this.formData.username == 'bob' && this.formData.password =='1234'){
+                this.$router.replace('/contacts')
+                this.succeessMessage = "Success"
+            }
+            else{
+                this.errorMessgae = "Username or Password is incorrect"
+                alert(this.errorMessgae)
+                    
+            }
+        }
+    },
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
