4f6a940a9ea71ed25c01a95a85059e3267c51d5c
Filmptz <you@example.com>
2021-04-24 16:55:40 +0700

    Layout

--- front_vite/src/views/Login.vue
+++ front_vite/src/views/Login.vue
@@ -1,18 +1,27 @@
 <template>
     <div class="container">
-        <h1>Log In</h1>
-        <br><br>
+        <div class="card" style="margin:13% 30% 5% 30%; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);transition: 0.3s; border-radius: 10px;">
+  <div class="card-header" style="background-color:#777D63;color:white;">
+    Log in
+  </div>
+  <div class="card-body">
+      </div>
         <div class="row">
-            <div class="col-lg-3"/>
-            <div class="col-lg-6">
+            <div class="col-lg-2"/>
+            <div class="col-lg-8">
+                <label class="pull-left">User Account</label>
                 <input type="username" v-model="formData.username" class="form-control" placeholder="username"/> 
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <input type="password" v-model="formData.password" class="form-control" placeholder="password"/>
-                <br> 
-                <button class="btn btn-success btn-block full-width" @click="logIn">Log In</button>
+                <p class="text-left" v-if="formData.username.length == 0" style="color:red" >*please enter username</p>
+                <label class="pull-left" style="margin-top:5px;">Password</label>
+                <input type="password" v-model="formData.password" class="form-control" placeholder="password" @keyup.enter="logIn"/>
+                <p class="pull-left" v-if="formData.password.length == 0" style="color:red" >*please enter username</p>
+                <br>
+                <button class="btn  w-50" style="background-color:#E2D7B9; margin-bottom: 15%; color:black;" @click="logIn">Log In</button>
             </div>
             <div class="col-lg-3"/>
         </div>
+  </div>
+        
         
     </div>
 </template>
